c1047197667ba6c0551df55d85d5c9ed6e30b459
Joshua <joshuagross0801@gmail.com>
2024-05-07 15:41:57 +0100

    added customer.js, added class imports to index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Import Classes Here
-
-
-
-
-
-
-
-
-
+const Product = require('./classes/Product');
+const Cart = require('./classes/Cart');
+const Customer = require('./Customer');
 
 // DO NOT EDIT BELOW THIS LINE
 try {
